46a7010d3a873a1f2c909ccb2956c66b3002b022
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2026-09-17 20:30:16 -0300

    feat: add project command discovery

--- src/kardecagent/project/__init__.py
+++ src/kardecagent/project/__init__.py
@@ -1,3 +1,3 @@
 from .context import iter_project_files, project_snapshot
-
-__all__ = ["iter_project_files", "project_snapshot"]
+from .detector import ProjectProfile, detect_project, discover_command
+__all__ = ['iter_project_files', 'project_snapshot', 'ProjectProfile', 'detect_project', 'discover_command']
