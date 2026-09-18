3940be478a65e21916e2e480326a358deb208f3b
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2026-09-17 22:02:16 -0300

    feat: add project profiles and static html validator

--- src/kardecagent/project/__init__.py
+++ src/kardecagent/project/__init__.py
@@ -1,3 +1,14 @@
 from .context import iter_project_files, project_snapshot
 from .detector import ProjectProfile, detect_project, discover_command
-__all__ = ['iter_project_files', 'project_snapshot', 'ProjectProfile', 'detect_project', 'discover_command']
+from .validators import ValidationResult, validate_project, validate_static_html
+
+__all__ = [
+    "iter_project_files",
+    "project_snapshot",
+    "ProjectProfile",
+    "detect_project",
+    "discover_command",
+    "ValidationResult",
+    "validate_project",
+    "validate_static_html",
+]
