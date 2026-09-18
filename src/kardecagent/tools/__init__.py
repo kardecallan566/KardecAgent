75f81d26720a175ec3391f9fe2bf79a917eeca49
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2026-09-17 23:55:24 -0300

    feat: expose patch editing tool

--- src/kardecagent/tools/__init__.py
+++ src/kardecagent/tools/__init__.py
@@ -4,13 +4,14 @@ from .git import (
     git_has_uncommitted_changes, git_is_repo, git_log, git_status, rollback_to,
 )
 from .search import search_text
+from .patch import PatchResult, apply_unified_patch
 from .terminal import CommandResult, is_command_allowed, is_command_blocked, run_command
 from .web import WebPage, WebResult, fetch_web_page, search_web
 
 __all__ = [
     "ProjectFilesystem", "GitResult", "git_status", "git_diff", "git_log",
     "git_is_repo", "git_current_branch", "git_has_uncommitted_changes",
-    "create_checkpoint", "rollback_to", "search_text", "CommandResult",
+    "create_checkpoint", "rollback_to", "search_text", "PatchResult", "apply_unified_patch", "CommandResult",
     "is_command_allowed", "is_command_blocked", "run_command",
     "WebPage", "WebResult", "search_web", "fetch_web_page",
 ]
