97432de14c0f034f31f3898d4ef0a38d56ae51de
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2026-09-18 00:01:22 -0300

    feat: add logical subtask task board

---
+++ src/kardecagent/tasks/__init__.py
@@ -0,0 +1,11 @@
+from .task import Subtask, SubtaskStatus, TaskBoard, TaskBoardError
+from .manager import SubtaskManager, SubtaskDecompositionError
+
+__all__ = [
+    "Subtask",
+    "SubtaskStatus",
+    "TaskBoard",
+    "TaskBoardError",
+    "SubtaskManager",
+    "SubtaskDecompositionError",
+]
