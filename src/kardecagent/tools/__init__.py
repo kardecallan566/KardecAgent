522cdd8b363f428f358a16f560483f68ba4c0549
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2026-09-17 22:02:40 -0300

    fix: export git and terminal policy helpers

--- src/kardecagent/tools/__init__.py
+++ src/kardecagent/tools/__init__.py
@@ -1,6 +1,14 @@
 from .filesystem import ProjectFilesystem
-from .git import GitResult, create_checkpoint, git_diff, git_log, git_status, rollback_to
+from .git import (
+    GitResult, create_checkpoint, git_current_branch, git_diff,
+    git_has_uncommitted_changes, git_is_repo, git_log, git_status, rollback_to,
+)
 from .search import search_text
-from .terminal import CommandResult, is_command_blocked, run_command
+from .terminal import CommandResult, is_command_allowed, is_command_blocked, run_command
 
-__all__ = ['ProjectFilesystem','GitResult','git_status','git_diff','git_log','create_checkpoint','rollback_to','search_text','CommandResult','is_command_blocked','run_command']
+__all__ = [
+    "ProjectFilesystem", "GitResult", "git_status", "git_diff", "git_log",
+    "git_is_repo", "git_current_branch", "git_has_uncommitted_changes",
+    "create_checkpoint", "rollback_to", "search_text", "CommandResult",
+    "is_command_allowed", "is_command_blocked", "run_command",
+]
