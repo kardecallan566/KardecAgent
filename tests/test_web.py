fbb5a8339d6547dfe49bdb3e14224f1704add379
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2026-09-17 23:02:50 -0300

    test: add SSRF and unsafe URL protections

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,4 +1,4 @@
-from kardecagent.tools.web import search_web
+from kardecagent.tools.web import _validate_url, search_web
 
 
 def test_search_web_rejects_empty_query():
@@ -15,3 +15,27 @@ def test_search_web_rejects_invalid_result_limit():
     except ValueError:
         return
     raise AssertionError("expected ValueError")
+
+
+def test_web_blocks_non_http_scheme():
+    try:
+        _validate_url("file:///etc/passwd")
+    except ValueError:
+        return
+    raise AssertionError("expected ValueError")
+
+
+def test_web_blocks_localhost():
+    try:
+        _validate_url("http://localhost:8080")
+    except PermissionError:
+        return
+    raise AssertionError("expected PermissionError")
+
+
+def test_web_blocks_private_ip():
+    try:
+        _validate_url("http://127.0.0.1/")
+    except PermissionError:
+        return
+    raise AssertionError("expected PermissionError")
