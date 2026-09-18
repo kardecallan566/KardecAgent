2c2d9bd47f995cda70fbc4ddaf1d386b2c0e1d3f
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2026-09-18 00:45:41 -0300

    refactor: export git changed path helper

--- src/kardecagent/tools/__init__.py
+++ src/kardecagent/tools/__init__.py
@@ -1,7 +1,7 @@
 from .filesystem import ProjectFilesystem
 from .git import (
     GitResult, create_checkpoint, git_current_branch, git_diff,
-    git_has_uncommitted_changes, git_is_repo, git_log, git_status, rollback_to,
+    git_has_uncommitted_changes, git_is_repo, git_log, git_status, git_changed_paths, rollback_to,
 )
 from .search import search_text
 from .patch import PatchResult, apply_unified_patch
@@ -10,7 +10,7 @@ from .web import WebPage, WebResult, fetch_web_page, search_web
 
 __all__ = [
     "ProjectFilesystem", "GitResult", "git_status", "git_diff", "git_log",
-    "git_is_repo", "git_current_branch", "git_has_uncommitted_changes",
+    "git_is_repo", "git_current_branch", "git_has_uncommitted_changes", "git_changed_paths",
     "create_checkpoint", "rollback_to", "search_text", "PatchResult", "apply_unified_patch", "CommandResult",
     "is_command_allowed", "is_command_blocked", "run_command",
     "WebPage", "WebResult", "search_web", "fetch_web_page",
